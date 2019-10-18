c196d83d6b73bb5fef3c12d102511f1a45fc83cd
ot218255 <ot218255@falmouth.ac.uk>
2019-10-18 09:00:08 +0100

    Uploading this from the uni computer.

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    public float speed = 20;
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Update()
+    { 
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+           
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+
+        }
+       
+    }
+
+}
