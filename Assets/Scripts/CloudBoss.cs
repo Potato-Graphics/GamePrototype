63fb955c5824d5704eb8a77088c25be9bf9e6ad4
ot218255 <ot218255@falmouth.ac.uk>
2019-11-05 11:04:29 +0000

    For Harry.

---
+++ Assets/Scripts/CloudBoss.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudBoss : MonoBehaviour
+{
+
+    private Player thePlayer;
+    public float moveSpeed;
+    public float playerRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thePlayer = FindObjectOfType<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, thePlayer.transform.position, moveSpeed * Time.deltaTime);
+    }
+}
